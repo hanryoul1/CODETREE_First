7d1626cde851bc1bbfb0ea098336ba5142005660
hanryoul1 <1228gksfbf@naver.com>
2022-07-13 18:47:14 +0900

    두줄 출력

---
+++ Lesson01_출력/Step01_기본 출력/01_08.py
@@ -0,0 +1,12 @@
+#1번
+print("Hello students")
+print("Welcome to LeebrosCode!")
+
+#2번
+print("Hello students\nWelcome to LeebrosCode!")
+
+#3번
+print("""
+Hello students
+Welcome to LeebrosCode!
+""")
