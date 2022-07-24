aab0d9c00e946103dd1711652b0d0636aa4266a2
hanryoul1 <1228gksfbf@naver.com>
2022-07-25 02:31:35 +0900

    4번의 크기 비교

---
+++ Lesson04_조건문/Step08_비교 연산자와 조건문/08_03.py
@@ -0,0 +1,9 @@
+a = int(input())
+inp = input()
+arr = inp.split()
+b, c, d, e = int(arr[0]), int(arr[1]), int(arr[2]), int(arr[3])
+
+print(int(a > b))
+print(int(a > c))
+print(int(a > d))
+print(int(a > e))
